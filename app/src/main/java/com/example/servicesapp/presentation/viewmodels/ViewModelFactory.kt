d810b519db71dd4007feb6d51b33b16cdd314c7b
Roman <romazzz777@gmail.com>
2023-02-20 23:56:22 +0300

    Commit

--- app/src/main/java/com/example/servicesapp/presentation/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/example/servicesapp/presentation/viewmodels/ViewModelFactory.kt
@@ -2,20 +2,14 @@ package com.example.servicesapp.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.servicesapp.domain.GetServiceListUseCase
-import com.example.servicesapp.domain.GetServiceUseCase
-import java.lang.RuntimeException
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val getServiceUseCase: GetServiceUseCase,
-    private val getServiceListUseCase: GetServiceListUseCase
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass == MainViewModel::class.java) {
-            return MainViewModel(getServiceUseCase, getServiceListUseCase) as T
-        }
-        throw RuntimeException("Unknown view model class $modelClass")
+        return viewModelProviders[modelClass]?.get() as T
     }
 }
